e290df1105a2f9495cc0d38ebe4ba7a835cf9327
Timothy Johnson <timothy.johnson@broadcom.com>
2022-12-07 10:57:13 -0500

    Make test script more helpful
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- test.js
+++ test.js
@@ -3,23 +3,23 @@ const keytarRust = require("./keytar.node");
 
 (async () => {
     await keytarNode.setPassword("test", "user1", "password");
-    console.log(await keytarNode.getPassword("test", "user1"));
-    console.log(await keytarRust.getPassword("test", "user1"));
+    console.log("NN:", await keytarNode.getPassword("test", "user1"));
+    console.log("NR:", await keytarRust.getPassword("test", "user1"));
     await keytarRust.setPassword("test", "user2", "password");
-    console.log(await keytarNode.getPassword("test", "user2"));
-    console.log(await keytarRust.getPassword("test", "user2"));
+    console.log("RN:", await keytarNode.getPassword("test", "user2"));
+    console.log("RR:", await keytarRust.getPassword("test", "user2"));
 
-    await keytarNode.setPassword("test", "user3", "💖💖");
-    console.log(await keytarNode.getPassword("test", "user3"));
-    console.log(await keytarRust.getPassword("test", "user3"));
-    await keytarRust.setPassword("test", "user4", "💖💖");
-    console.log(await keytarNode.getPassword("test", "user4"));
-    console.log(await keytarRust.getPassword("test", "user4"));
+    await keytarNode.setPassword("test", "user3", "💖🦀");
+    console.log("NN:", await keytarNode.getPassword("test", "user3"));
+    console.log("NR:", await keytarRust.getPassword("test", "user3"));
+    await keytarRust.setPassword("test", "user4", "💖🦀");
+    console.log("RN:", await keytarNode.getPassword("test", "user4"));
+    console.log("RR:", await keytarRust.getPassword("test", "user4"));
 
     await keytarNode.setPassword("test", "user5", "password💖");
-    console.log(await keytarNode.getPassword("test", "user5"));
-    console.log(await keytarRust.getPassword("test", "user5"));
+    console.log("NN:", await keytarNode.getPassword("test", "user5"));
+    console.log("NR:", await keytarRust.getPassword("test", "user5"));
     await keytarRust.setPassword("test", "user6", "password💖");
-    console.log(await keytarNode.getPassword("test", "user6"));
-    console.log(await keytarRust.getPassword("test", "user6"));
+    console.log("RN:", await keytarNode.getPassword("test", "user6"));
+    console.log("RR:", await keytarRust.getPassword("test", "user6"));
 })();
