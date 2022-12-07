ea9189859ba18f2826e16e187689ea2b848eeb58
Timothy Johnson <timothy.johnson@broadcom.com>
2022-12-06 19:13:26 -0500

    Fix encoding issues on Windows
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,49 @@ use napi::bindgen_prelude::*;
 use napi_derive::napi;
 
 #[cfg(windows)]
+use byteorder::{ByteOrder, LittleEndian};
+
+#[cfg(windows)]
 fn keyringEntry(service: &str, account: &str) -> Entry {
     let target = format!("{}/{}", &service, &account);
-    Entry::new_with_target(&target, &target, &account)
+    Entry::new_with_target(&target, &target, account)
 }
 
 #[cfg(unix)]
 fn keyringEntry(service: &str, account: &str) -> Entry {
-    Entry::new(&service, &account)
+    Entry::new(service, account)
+}
+
+#[cfg(windows)]
+fn decodePassword(password: &str) -> String {
+    let blob_u16: Vec<u16> = password.encode_utf16().collect();
+    let mut blob = vec![0; blob_u16.len() * 2];
+    LittleEndian::write_u16_into(&blob_u16, &mut blob);
+    String::from_utf8_lossy(&blob).to_string()
+}
+
+#[cfg(unix)]
+fn decodePassword(password: &str) -> String {
+    password.to_string()
+}
+
+#[cfg(windows)]
+fn encodePassword(password: &str) -> String {
+    let blob = password.as_bytes();
+    let mut blob_u16 = vec![0; blob.len() / 2];
+    LittleEndian::read_u16_into(blob, &mut blob_u16);
+    String::from_utf16_lossy(&blob_u16)
+}
+
+#[cfg(unix)]
+fn encodePassword(password: &str) -> String {
+    password.to_string()
 }
 
 #[napi]
 fn getPassword(service: String, account: String) -> Result<Option<String>> {
     match keyringEntry(&service, &account).get_password() {
-        Ok(password) => Ok(Some(password)),
+        Ok(password) => Ok(Some(decodePassword(&password))),
         Err(Error::NoEntry) => Ok(None),
         Err(e) => Err(napi::Error::new(Status::InvalidArg, e.to_string()))
     }
@@ -24,7 +53,7 @@ fn getPassword(service: String, account: String) -> Result<Option<String>> {
 
 #[napi]
 fn setPassword(service: String, account: String, password: String) -> Result<()> {
-    match keyringEntry(&service, &account).set_password(&password) {
+    match keyringEntry(&service, &account).set_password(&encodePassword(&password)) {
         Ok(()) => Ok(()),
         Err(e) => Err(napi::Error::new(Status::InvalidArg, e.to_string()))
     }
@@ -36,5 +65,21 @@ fn deletePassword(service: String, account: String) -> Result<bool> {
         Ok(_) => Ok(true),
         Err(Error::NoEntry) => Ok(false),
         Err(e) => Err(napi::Error::new(Status::InvalidArg, e.to_string()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_keyring() {
+        let testService = "test";
+        let testAccount = "user";
+        let testPassword = "password💖";
+        setPassword(testService.to_string(), testAccount.to_string(), testPassword.to_string()).unwrap();
+        assert_eq!(getPassword(testService.to_string(), testAccount.to_string()).unwrap(), Some(testPassword.to_string()));
+        assert_eq!(deletePassword(testService.to_string(), testAccount.to_string()).unwrap(), true);
+        assert_eq!(getPassword(testService.to_string(), testAccount.to_string()).unwrap(), None);
     }
 }
--- test.js
+++ test.js
@@ -1,4 +1,17 @@
-const keytar = require("./keytar.node");
-// console.log(Buffer.from(keytar.getPassword("Zowe", "secure_config_props"), "utf16le").toString());
-console.log(keytar.getPassword("Zowe", "secure_config_props"));
-// keytar.setPassword("Zowe", "test", "hello");
+const keytarNode = require("../zowe/zowe-cli/node_modules/keytar");
+const keytarRust = require("./keytar.node");
+
+(async () => {
+    await keytarNode.setPassword("test", "user1", "password");
+    console.log(await keytarNode.getPassword("test", "user1"));
+    console.log(keytarRust.getPassword("test", "user1"));
+    await keytarRust.setPassword("test", "user2", "password");
+    console.log(await keytarNode.getPassword("test", "user2"));
+    console.log(keytarRust.getPassword("test", "user2"));
+    await keytarNode.setPassword("test", "user3", "password💖");
+    console.log(await keytarNode.getPassword("test", "user3"));
+    console.log(keytarRust.getPassword("test", "user3"));
+    await keytarRust.setPassword("test", "user4", "password💖");
+    console.log(await keytarNode.getPassword("test", "user4"));
+    console.log(keytarRust.getPassword("test", "user4"));
+})();
